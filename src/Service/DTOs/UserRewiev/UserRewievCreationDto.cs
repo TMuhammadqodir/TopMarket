d4752d441c483bb6bf4b52bee19333d1730f5358
Saidkamolxon <saidkamolxon1997@gmail.com>
2024-05-22 08:25:30 +0500

    Enums: Name fixed

--- src/Service/DTOs/UserRewiev/UserRewievCreationDto.cs
+++ src/Service/DTOs/UserRewiev/UserRewievCreationDto.cs
@@ -6,6 +6,6 @@ public class UserRewievCreationDto
 {
     public long UserId { get; set; }
     public long OrderProductId { get; set; }
-    public Rating RatingValue { get; set; }
+    public ERating RatingValue { get; set; }
     public string Comment { get; set; }
 }
--- src/Service/DTOs/Users/UserUpdateDto.cs
+++ src/Service/DTOs/Users/UserUpdateDto.cs
@@ -9,5 +9,5 @@ public class UserUpdateDto
     public string LastName { get; set; } = string.Empty;
     public string Email { get; set; } = string.Empty;
     public string Phone { get; set; } = string.Empty;
-    public UserRole UserRole { get; set; }
+    public EUserRole UserRole { get; set; }
 }
