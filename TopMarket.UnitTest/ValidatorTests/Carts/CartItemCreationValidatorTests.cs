c126215d30bd0bdd5045b8f01ca2870a04aa7b50
Saidkamolxon <saidkamolxon1997@gmail.com>
2024-05-24 12:13:33 +0500

    UnitTest: Added tests for CartItemCreationValidator & CartItemUpdateValidator

---
+++ TopMarket.UnitTest/ValidatorTests/Carts/CartItemCreationValidatorTests.cs
@@ -0,0 +1,48 @@
+﻿using Service.DTOs.Carts;
+
+namespace Service.Validators.Carts;
+
+public class CartItemCreationValidatorTests
+{
+    private readonly CartItemCreationValidator validator;
+    
+    public CartItemCreationValidatorTests()
+    {
+        this.validator = new CartItemCreationValidator();
+    }
+
+    [Theory]
+    [InlineData(0, 0, 0, 0)]
+    [InlineData(1, 0, 0, 0)]
+    [InlineData(0, 1, 0, 0)]
+    [InlineData(0, 0, 1, 0)]
+    [InlineData(0, 0, 0, 1)]
+    public void ShouldBeEqualToFalse(long cartId, long productItemId, decimal price, float quantity)
+    {
+        var cartItem = new CartItemCreationDto
+        {
+            CartId = cartId,
+            ProductItemId = productItemId,
+            Price = price,
+            Quantity = quantity
+        };
+        
+        Assert.False(this.validator.Validate(cartItem).IsValid);
+    }
+
+    [Theory]
+    [InlineData(1, 1, 0.01d, 0.1f)]
+    [InlineData(1, 12, 123, 1234)]
+    public void ShouldBeEqualToTrue(long cartId, long productItemId, decimal price, float quantity)
+    {
+        var cartItem = new CartItemCreationDto
+        {
+            CartId = cartId,
+            ProductItemId = productItemId,
+            Price = price,
+            Quantity = quantity
+        };
+
+        Assert.True(this.validator.Validate(cartItem).IsValid);
+    }
+}
---
+++ TopMarket.UnitTest/ValidatorTests/Carts/CartItemUpdateValidatorTests.cs
@@ -0,0 +1,51 @@
+﻿using Service.DTOs.Carts;
+
+namespace Service.Validators.Carts;
+
+public class CartItemUpdateValidatorTests
+{
+    private readonly CartItemUpdateValidator validator;
+
+    public CartItemUpdateValidatorTests()
+    {
+        this.validator = new CartItemUpdateValidator();
+    }
+
+    [Theory]
+    [InlineData(0, 0, 0, 0, 0)]
+    [InlineData(1, 0, 0, 0, 0)]
+    [InlineData(0, 1, 0, 0, 0)]
+    [InlineData(0, 0, 1, 0, 0)]
+    [InlineData(0, 0, 0, 1, 0)]
+    [InlineData(0, 0, 0, 0, 1)]
+    public void ShouldBeEqualToFalse(long id, int cartId, long productItemId, decimal price, float quantity)
+    {
+        var cartItem = new CartItemUpdateDto
+        {
+            Id = id,
+            CartId = cartId,
+            ProductItemId = productItemId,
+            Price = price,
+            Quantity = quantity
+        };
+
+        Assert.False(this.validator.Validate(cartItem).IsValid);
+    }
+
+    [Theory]
+    [InlineData(1, 2, 3, 0.04d, 0.5f)]
+    [InlineData(1, 12, 123, 1234, 12345)]
+    public void ShouldBeEqualToTrue(long id, int cartId, long productItemId, decimal price, float quantity)
+    {
+        var cartItem = new CartItemUpdateDto
+        {
+            Id = id,
+            CartId = cartId,
+            ProductItemId = productItemId,
+            Price = price,
+            Quantity = quantity
+        };
+
+        Assert.True(this.validator.Validate(cartItem).IsValid);
+    }
+}
