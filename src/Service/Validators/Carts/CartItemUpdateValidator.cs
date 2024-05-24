fea0a902fe1d5283d860e56a7ce1eeac91b4ff6f
Saidkamolxon <saidkamolxon1997@gmail.com>
2024-05-24 10:34:31 +0500

    Carts: Added validators

---
+++ src/Service/Validators/Carts/CartItemUpdateValidator.cs
@@ -0,0 +1,26 @@
+﻿using FluentValidation;
+using Service.DTOs.Carts;
+
+namespace Service.Validators.Carts;
+
+public class CartItemUpdateValidator : AbstractValidator<CartItemUpdateDto>
+{
+    public CartItemUpdateValidator()
+    {
+        RuleFor(dto => dto.Quantity)
+            .NotEqual(0)
+            .WithMessage("Quantity should NOT be equal to 0.");
+
+        RuleFor(dto => dto.Price)
+            .NotEqual(0)
+            .WithMessage("Price should NOT be equal to 0");
+
+        RuleFor(dto => dto.ProductItemId)
+            .NotEqual(0)
+            .WithMessage("Product Item Id is NOT valid");
+
+        RuleFor(dto => dto.CartId)
+            .NotEqual(0)
+            .WithMessage("Cart Id is NOT valid");
+    }
+}
