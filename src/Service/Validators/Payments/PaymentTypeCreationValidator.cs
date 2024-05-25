e8be1c2b44af3b4ed44c86fe900adea886387746
Saidkamolxon <saidkamolxon1997@gmail.com>
2024-05-25 20:26:09 +0500

    Payments: Added validators

---
+++ src/Service/Validators/Payments/PaymentTypeCreationValidator.cs
@@ -0,0 +1,14 @@
+﻿using FluentValidation;
+using Service.DTOs.Payments.PaymentType;
+
+namespace Service.Validators.Payments;
+
+public class PaymentTypeCreationValidator : AbstractValidator<PaymentTypeCreationDto>
+{
+    public PaymentTypeCreationValidator()
+    {
+        RuleFor(pt => pt.Value)
+            .NotEmpty()
+                .WithMessage("Value should NOT be empty.");
+    }
+}
