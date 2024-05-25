c55cb4aaa2ca3999fdd9a8fbc83a00877ac2d8c6
Saidkamolxon <saidkamolxon1997@gmail.com>
2024-05-25 22:14:16 +0500

    OrderStatus Validators: Added unit tests

---
+++ TopMarket.UnitTest/ValidatorTests/OrderStatuses/OrderStatusCreationValidatorTests.cs
@@ -0,0 +1,52 @@
+﻿using Service.DTOs.OrderStatuses;
+
+namespace Service.Validators.OrderStatuses;
+
+public class OrderStatusCreationValidatorTests
+{
+    private readonly OrderStatusCreationValidator validator;
+
+    public OrderStatusCreationValidatorTests()
+    {
+        this.validator = new OrderStatusCreationValidator();
+    }
+
+    [Theory]
+    [InlineData(default)]
+    [InlineData("")]
+    [InlineData("abc")] // checking for min length
+    public void CheckingForName_ShouldNotBeValid(string name)
+    {
+        var orderStatus = new OrderStatusCreationDto
+        {
+            Name = name
+        };
+
+        Assert.False(this.validator.Validate(orderStatus).IsValid);
+    }
+
+    [Theory]
+    [InlineData('a')]
+    public void CheckingForNameMaxLength_ShouldNotBeValid(char c)
+    {
+        var orderStatus = new OrderStatusCreationDto
+        {
+            Name = new string(c, 101)
+        };
+
+        Assert.False(this.validator.Validate(orderStatus).IsValid);
+    }
+
+    [Theory]
+    [InlineData("abcd")]
+    [InlineData("OrderStatus1")]
+    public void CheckingForName_ShouldBeValid(string name)
+    {
+        var orderStatus = new OrderStatusCreationDto
+        {
+            Name = name
+        };
+
+        Assert.True(this.validator.Validate(orderStatus).IsValid);
+    }
+}
---
+++ TopMarket.UnitTest/ValidatorTests/OrderStatuses/OrderStatusUpdateValidatorTests.cs
@@ -0,0 +1,56 @@
+﻿using Service.DTOs.OrderStatuses;
+
+namespace Service.Validators.OrderStatuses;
+
+public class OrderStatusUpdateValidatorTests
+{
+    private readonly OrderStatusUpdateValidator validator;
+
+    public OrderStatusUpdateValidatorTests()
+    {
+        this.validator = new OrderStatusUpdateValidator();
+    }
+
+    [Theory]
+    [InlineData(default, default)]
+    [InlineData(0L, "")]
+    [InlineData(1L, "")]
+    [InlineData(1L, "abc")]// checking for min length
+    public void CheckingForName_ShouldNotBeValid(long id, string name)
+    {
+        var orderStatus = new OrderStatusUpdateDto
+        {
+            Id = id,
+            Name = name
+        };
+
+        Assert.False(this.validator.Validate(orderStatus).IsValid);
+    }
+
+    [Theory]
+    [InlineData(1L, 'a')]
+    public void CheckingForNameMaxLength_ShouldNotBeValid(long id, char c)
+    {
+        var orderStatus = new OrderStatusUpdateDto
+        {
+            Id = id,
+            Name = new string(c, 101)
+        };
+
+        Assert.False(this.validator.Validate(orderStatus).IsValid);
+    }
+
+    [Theory]
+    [InlineData(1L, "abcd")]
+    [InlineData(2L, "OrderStatus1")]
+    public void CheckingForName_ShouldBeValid(long id, string name)
+    {
+        var orderStatus = new OrderStatusUpdateDto
+        {
+            Id = id,
+            Name = name
+        };
+
+        Assert.True(this.validator.Validate(orderStatus).IsValid);
+    }
+}
