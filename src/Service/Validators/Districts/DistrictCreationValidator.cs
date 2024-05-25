e17922e2fe527dc566aa91191efd30c05a7bc39e
Muhammadqodir Turdiyev <118986233+TMuhammadqodir@users.noreply.github.com>
2024-05-25 23:25:19 +0500

    Clean code for validators

--- src/Service/Validators/Districts/DistrictCreationValidator.cs
+++ src/Service/Validators/Districts/DistrictCreationValidator.cs
@@ -9,30 +9,30 @@ public class DistrictCreationValidator : AbstractValidator<DistrictCreationDto>
     {
         RuleFor(item => item.NameUz)
             .NotEmpty()
-            .WithMessage("Country cannot be empty")
+                .WithMessage("Country cannot be empty")
             .MaximumLength(128)
-            .WithMessage("This lastname very long")
+                .WithMessage("This lastname very long")
             .MinimumLength(2)
-            .WithMessage("This lastname very short");
+                .WithMessage("This lastname very short");
 
         RuleFor(item => item.NameOz)
             .NotEmpty()
-            .WithMessage("Country code cannot be empty")
+                .WithMessage("Country code cannot be empty")
             .MaximumLength(128)
-            .WithMessage("This lastname very long")
+                .WithMessage("This lastname very long")
             .MinimumLength(2)
-            .WithMessage("This lastname very short");
+                .WithMessage("This lastname very short");
 
         RuleFor(item => item.NameRu)
             .NotEmpty()
-            .WithMessage("Country code cannot be empty")
+                .WithMessage("Country code cannot be empty")
             .MaximumLength(128)
-            .WithMessage("This lastname very long")
+                .WithMessage("This lastname very long")
             .MinimumLength(2)
-            .WithMessage("This lastname very short");
+                .WithMessage("This lastname very short");
 
         RuleFor(item => item.RegionId)
             .GreaterThan(0)
-            .WithMessage("id cannot be negative or 0");
+                .WithMessage("id cannot be negative or 0");
     }
 }
