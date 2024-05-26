272f34ef232c633de67355530362571566220f79
Muhammadqodir Turdiyev <118986233+TMuhammadqodir@users.noreply.github.com>
2024-05-27 00:28:21 +0500

    Add ProductValidator test

--- TopMarket.UnitTest/ValidatorTests/Products/ProductCreationValidatorTest.cs
+++ TopMarket.UnitTest/ValidatorTests/Products/ProductCreationValidatorTest.cs
@@ -1,12 +1,74 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Domain.Entities.ProductFolder;
+using Service.DTOs.Products;
+using Service.Validators.Products;
+using System.Xml.Linq;
 
-namespace TopMarket.UnitTest.ValidatorTests.Products
+namespace TopMarket.UnitTest.ValidatorTests.Products;
+
+public class ProductCreationValidatorTest
 {
-    internal class ProductCreationValidatorTest
+    private ProductCreationValidator productCreationValidator;
+    public ProductCreationValidatorTest()
     {
+        this.productCreationValidator = new ProductCreationValidator();
+    }
+
+    [Theory]
+    [InlineData("uzbekistan", "", 1L)]
+    [InlineData("", "uz", 2L)]
+    [InlineData("", "", 0)]
+    [InlineData("fd", "fd", 0)]
+    [InlineData(null, "udd", 1L)]
+    [InlineData("uz",null, null)]
+    [InlineData("", "ds", 2)]
+    [InlineData(null, null, null)]
+
+    public void ShouldReturnFalseForName(string name, string description, long categoryId)
+    {
+        var product = new ProductCreationDto()
+        {
+            Name = name,
+            Description = description,
+            CategoryId = categoryId
+        };
+
+        var result = productCreationValidator.Validate(product);
+
+        Assert.False(result.IsValid);
+    }
+
+    [Theory]
+    [InlineData('u')]
+    public void ShouldReturnFalseForNameLenth(char character)
+    {
+        var product = new ProductCreationDto()
+        {
+            Name = new string(character, 129),
+            Description = new string(character, 513),
+            CategoryId = 5
+        };
+
+        var result = productCreationValidator.Validate(product);
+
+        Assert.False(result.IsValid);
+    }
+
+    [Theory]
+    [InlineData("uzbekistan", "dw", 1L)]
+    [InlineData("dew", "uz", 2L)]
+    [InlineData("de", "dwd", 2L)]
+
+    public void ShouldReturnTrueName(string name, string description, long categoryId)
+    {
+        var product = new ProductCreationDto()
+        {
+            Name = name,
+            Description = description,
+            CategoryId = categoryId
+        };
+
+        var result = productCreationValidator.Validate(product);
+
+        Assert.True(result.IsValid);
     }
 }
