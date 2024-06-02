cc2b32612aafa03c0a3ef94c6dca09d73a15bb49
Saidkamolxon <saidkamolxon1997@gmail.com>
2024-06-02 18:40:37 +0500

    PromotionCategory: Added Validators

---
+++ src/Service/Validators/PromotionCategories/PromotionCategoryCreationValidator.cs
@@ -0,0 +1,16 @@
+﻿using FluentValidation;
+using Service.DTOs.PromotionCategories;
+
+namespace Service.Validators.PromotionCategories;
+
+public class PromotionCategoryCreationValidator : AbstractValidator<PromotionCategoryCreationDto>
+{
+    public PromotionCategoryCreationValidator()
+    {
+        RuleFor(pc => pc.CategoryId)
+            .NotEmpty();
+
+        RuleFor(pc => pc.PromotionId)
+            .NotEmpty();
+    }
+}
